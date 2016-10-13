f75c92a041b496384d3769ca30892159503b39a7
barnameh <farimahd@gmail.com>
2016-10-13 17:33:24 -0400

    Modified the data structure for advice data to be in a hash with keys representing hand type and values being arrays of array containing the advice key. Modified the code to accomodate changes made to the data structure. Functionality remains the same.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,63 +1,64 @@
-
-hard =
-    [
-        ## 2     3     4     5     6     7     8     9     T     A    ET    EA
-        [ "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H"],   ## Hard  5
-        [ "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H"],   ## Hard  6
-        [ "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H"],   ## Hard  7
-        [ "H",  "H",  "H", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Hard  8
-        ["DH", "DH", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Hard  9
-        ["DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH",  "H",  "H"],   ## Hard 10
-        ["DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH"],   ## Hard 11
-        [ "H",  "H",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 12
-        [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 13
-        [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 14
-        [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 15
-        [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 16
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 17
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 18
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 19
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 20
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"]    ## Hard 21
-      ]
-soft =
-    [
-        ## 2     3     4     5     6     7     8     9     T     A    ET    EA
-        [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 13
-        [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 14
-        [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 15
-        [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 16
-        ["DH", "DH", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 17
-        [ "S", "DS", "DS", "DS", "DS",  "S",  "S",  "H",  "H",  "S"],   ## Soft 18
-        [ "S",  "S",  "S",  "S", "DS",  "S",  "S",  "S",  "S",  "S"],   ## Soft 19
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Soft 20
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"]    ## Soft 21
-      ]
-pair =
-      [
-        ## 2     3     4     5     6     7     8     9     T     A    ET    EA
-        ["P",   "P",  "P",  "P",  "P",  "P",  "H",  "H",  "H",  "H"],   ## 2,2
-        ["P",   "P",  "P",  "P",  "P",  "P",  "P",  "H",  "H",  "H"],   ## 3,3
-        [ "H",  "H",  "P",  "P",  "P",  "H",  "H",  "H",  "H",  "H"],   ## 4,4
-        ["DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH",  "H",  "H"],   ## 5,5
-        [ "P",  "P",  "P",  "P",  "P",  "P",  "H",  "H",  "H",  "H"],   ## 6,6
-        [ "P",  "P",  "P",  "P",  "P",  "P",  "P",  "H",  "S",  "H"],   ## 7,7
-        [ "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P"],   ## 8,8
-        [ "P",  "P",  "P",  "P",  "P",  "S",  "P",  "P",  "S",  "S"],   ## 9,9
-        [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## T,T
-        [ "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P"]    ## A,A
-    ]
-
+advice_data =
+{
+  "hard" =>
+  [
+    ## 2     3     4     5     6     7     8     9     T     A    ET    EA
+    [ "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H"],   ## Hard  5
+    [ "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H"],   ## Hard  6
+    [ "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H",  "H"],   ## Hard  7
+    [ "H",  "H",  "H", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Hard  8
+    ["DH", "DH", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Hard  9
+    ["DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH",  "H",  "H"],   ## Hard 10
+    ["DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH"],   ## Hard 11
+    [ "H",  "H",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 12
+    [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 13
+    [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 14
+    [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 15
+    [ "S",  "S",  "S",  "S",  "S",  "H",  "H",  "H",  "H",  "H"],   ## Hard 16
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 17
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 18
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 19
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Hard 20
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"]    ## Hard 21
+  ],
+  "soft" =>
+  [
+    ## 2     3     4     5     6     7     8     9     T     A    ET    EA
+    [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 13
+    [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 14
+    [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 15
+    [ "H",  "H", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 16
+    ["DH", "DH", "DH", "DH", "DH",  "H",  "H",  "H",  "H",  "H"],   ## Soft 17
+    [ "S", "DS", "DS", "DS", "DS",  "S",  "S",  "H",  "H",  "S"],   ## Soft 18
+    [ "S",  "S",  "S",  "S", "DS",  "S",  "S",  "S",  "S",  "S"],   ## Soft 19
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## Soft 20
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"]    ## Soft 21
+  ],
+  "pair" =>
+  [
+    ## 2     3     4     5     6     7     8     9     T     A    ET    EA
+    ["P",   "P",  "P",  "P",  "P",  "P",  "H",  "H",  "H",  "H"],   ## 2,2
+    ["P",   "P",  "P",  "P",  "P",  "P",  "P",  "H",  "H",  "H"],   ## 3,3
+    [ "H",  "H",  "P",  "P",  "P",  "H",  "H",  "H",  "H",  "H"],   ## 4,4
+    ["DH", "DH", "DH", "DH", "DH", "DH", "DH", "DH",  "H",  "H"],   ## 5,5
+    [ "P",  "P",  "P",  "P",  "P",  "P",  "H",  "H",  "H",  "H"],   ## 6,6
+    [ "P",  "P",  "P",  "P",  "P",  "P",  "P",  "H",  "S",  "H"],   ## 7,7
+    [ "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P"],   ## 8,8
+    [ "P",  "P",  "P",  "P",  "P",  "S",  "P",  "P",  "S",  "S"],   ## 9,9
+    [ "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S",  "S"],   ## T,T
+    [ "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P",  "P"]    ## A,A
+  ]
+}
 
 advice_detail = { "H" =>	"Hit",
-                  "S" => 	"Stand",
-                  "P" =>	"Split",
-                  "DH" =>	"Double if possible, otherwise Hit",
-                  "DS" =>	"Double if possible, otherwise Stand"
-                }
+  "S" => 	"Stand",
+  "P" =>	"Split",
+  "DH" =>	"Double if possible, otherwise Hit",
+  "DS" =>	"Double if possible, otherwise Stand"
+}
 
 def get_value_for_card(card)
-case card.upcase
+  case card.upcase
   when "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
     return card.to_i
   when "J", "Q", "K"
@@ -108,17 +109,21 @@ puts "valid input dealers_card is #{dealers_card} translated to #{dealers_card_v
 
 
 if card1.eql?(card2) == true
-  # got a pair
-  advice = pair[card1_value - 2][dealers_card_value - 2]
-  puts "getting advice from pair [ #{card1_value - 2}, #{dealers_card_value - 2}] "
+  hand_type = "pair"
+  user_index = card1_value - 2
+  dealer_index = dealers_card_value - 2
 elsif card1 == "A" || card2 == "A"
-  # soft hand
-  advice = soft[(card1_value + card2_value) - 13][dealers_card_value - 2]
+  hand_type = "soft"
+  user_index = (card1_value + card2_value) - 13
+  dealer_index = dealers_card_value - 2
 else
-  # hard hand
-  advice = hard[(card1_value + card2_value) - 5][dealers_card_value - 2]
+  hand_type = "hard"
+  user_index = (card1_value + card2_value) - 5
+  dealer_index = dealers_card_value - 2
 end
 
-puts " advice key is #{advice}"
+advice_key = advice_data[hand_type][user_index][dealer_index]
 
-puts "Your optimal move is to #{advice_detail[advice]}."
+puts " advice key is #{advice_key}"
+
+puts "Your optimal move is to #{advice_detail[advice_key]}."
